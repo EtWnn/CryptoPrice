8e9d6f239875ed291157244ef3d650e11b7172d9
EtWnn <eti1ww@gmail.com>
2021-04-01 10:10:10 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+about = {}
+with open(os.path.join(this_directory, 'CryptoPrice/__init__.py'), encoding='utf-8') as f:
+    exec(f.read(), about)
+
+setup(
+    name='CryptoPrice',
+    version=about['__version__'],
+    packages=['CryptoPrice'],
+    url='https://github.com/EtWnn/CryptoPrice',
+    author='EtWnn',
+    author_email='',
+    license='MIT',
+    description='Library to retrieve price or candle history of crypto assets using multiple sources',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    install_requires=['requests'],
+    keywords='eth bsc price ohlc candle history API Binance Kucoin',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
