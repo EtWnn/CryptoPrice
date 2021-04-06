71498c4880b840221b275a79771a613cb0053620
EtWnn <eti1ww@gmail.com>
2021-04-06 18:41:34 +0200

    write _explore_assets_path as BFS

--- CryptoPrice/retrievers/MetaRetriever.py
+++ CryptoPrice/retrievers/MetaRetriever.py
@@ -1,3 +1,4 @@
+from queue import Queue
 from typing import List, Optional, Dict, Tuple
 
 from CryptoPrice.common.prices import Price, MetaPrice
@@ -43,10 +44,10 @@ class MetaRetriever(AbstractRetriever):
                 return price
 
     def get_path_price(self, asset: str, ref_asset: str, timestamp: int,
-                       preferred_assets: Optional[List[str]] = None) -> Optional[MetaPrice]:
+                       preferred_assets: Optional[List[str]] = None, max_depth: int = 3) -> Optional[MetaPrice]:
         """
-        Will get the closest price possible in time for a trading pair asset/ref asset. If no price is found, return
-        None
+        Iterator that will get the closest price possible in time for a trading pair asset/ref asset.
+        If no price is found, return None
 
         :param asset: name of the asset in the trading pair (ex 'BTC' in 'BTCUSDT')
         :type asset: str
@@ -57,6 +58,8 @@ class MetaRetriever(AbstractRetriever):
         :param preferred_assets: list of assets to construct the price path from. If None, default value is
             ['BTC', 'USDT', 'BUSD', 'ETH']
         :type preferred_assets: Optional[List[str]]
+        :param max_depth: maximum number of trading pair to use, default 3
+        :type max_depth: int
         :return: the price closest in time found or None if no price found
         :rtype: Optional[MetaPrice]
         """
@@ -71,7 +74,8 @@ class MetaRetriever(AbstractRetriever):
 
         seen_assets = [asset]
         current_path = []
-        for assets_p, trade_p in self._explore_assets_path(ref_asset, assets_neighbours, seen_assets, current_path):
+        for assets_p, trade_p in self._explore_assets_path(ref_asset, assets_neighbours, seen_assets,
+                                                           current_path, max_depth=max_depth):
             yield self.create_meta_price(timestamp, assets_p, trade_p)
 
     def create_meta_price(self, timestamp: int, seen_assets: List[str],
@@ -91,7 +95,7 @@ class MetaRetriever(AbstractRetriever):
         cumulated_price = 1.
         prices = []
         for i, pair in enumerate(trading_path):
-            current_asset, next_asset = seen_assets[i:i+2]
+            current_asset, next_asset = seen_assets[i:i + 2]
             price = self.retrievers[pair.source].get_closest_price(pair.asset, pair.ref_asset, timestamp)
             if price is None:
                 return
@@ -124,8 +128,10 @@ class MetaRetriever(AbstractRetriever):
                         assets_neighbours[pair.ref_asset] = {pair.asset: [pair]}
         return assets_neighbours
 
-    def _explore_assets_path(self, target_asset: str, assets_neighbours: Dict, seen_assets: List[str],
-                             current_path: List[TradingPair]) -> Tuple[List[str], List[TradingPair]]:
+    @staticmethod
+    def _explore_assets_path(target_asset: str, assets_neighbours: Dict,
+                             seen_assets: List[str], current_path: List[TradingPair],
+                             max_depth: int = 3) -> Tuple[List[str], List[TradingPair]]:
         """
         Iterator that will explore the trading possibilities to link one asset to another one through a trading path
 
@@ -137,16 +143,20 @@ class MetaRetriever(AbstractRetriever):
         :type seen_assets: List[str]
         :param current_path: trading pair to use (in order) to follow the path to the target asset
         :type current_path: List[TradingPair]
+        :param max_depth: maximum number of trading pair to use, default 3
+        :type max_depth: int
         :return: list of assets to explore and the trading path to take
         :rtype: Tuple[List[str], List[TradingPair]]
         """
-        current_asset = seen_assets[-1]
-        for next_asset in assets_neighbours[current_asset]:
-            if next_asset not in seen_assets:
-                for pair in assets_neighbours[current_asset][next_asset]:
-                    if next_asset == target_asset:
-                        yield seen_assets + [next_asset], current_path + [pair]
-                    else:
-                        for result in self._explore_assets_path(target_asset, assets_neighbours,
-                                                                seen_assets + [next_asset], current_path + [pair]):
-                            yield result
+        to_explore = Queue()
+        to_explore.put((seen_assets, current_path))
+        while to_explore.qsize():
+            seen_assets, current_path = to_explore.get()
+            current_asset = seen_assets[-1]
+            for next_asset in assets_neighbours[current_asset]:
+                if next_asset not in seen_assets:
+                    for pair in assets_neighbours[current_asset][next_asset]:
+                        if next_asset == target_asset:
+                            yield seen_assets + [next_asset], current_path + [pair]
+                        elif len(current_path) + 1 < max_depth:
+                            to_explore.put((seen_assets + [next_asset], current_path + [pair]))
