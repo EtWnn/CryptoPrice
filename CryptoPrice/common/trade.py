7918b9868953d9f66f0c3d4349274c44af034ebd
EtWnn <eti1ww@gmail.com>
2021-04-06 12:12:55 +0200

    add trading pair equal comparison

--- CryptoPrice/common/trade.py
+++ CryptoPrice/common/trade.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 
 
@@ -7,3 +8,6 @@ class TradingPair:
     asset: str
     ref_asset: str
     source: str
+
+    def __eq__(self, other: TradingPair) -> bool:
+        return self.asset == other.asset and self.ref_asset == other.ref_asset
