7511d741418af40196c585b5eed5aa66ac6b6798
EtWnn <eti1ww@gmail.com>
2021-04-19 09:17:47 +0200

    move the default retriever to a function

--- CryptoPrice/__init__.py
+++ CryptoPrice/__init__.py
@@ -5,4 +5,12 @@ from CryptoPrice.retrievers.BinanceRetriever import BinanceRetriever
 from CryptoPrice.retrievers.KucoinRetriever import KucoinRetriever
 from CryptoPrice.retrievers.MetaRetriever import MetaRetriever
 
-retriever = MetaRetriever([BinanceRetriever(), KucoinRetriever()])
+
+def get_default_retriever() -> MetaRetriever:
+    """
+    Provides a hands on price retriever made from the default BinanceRetriever and the default KucoinRetriever
+
+    :return: the meta retriever constructed
+    :rtype: MetaRetriever
+    """
+    return MetaRetriever([BinanceRetriever(), KucoinRetriever()])
