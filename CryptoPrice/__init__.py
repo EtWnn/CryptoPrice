1f5073a5fbb095ed6e4fc7f2544a395694965882
EtWnn <eti1ww@gmail.com>
2021-06-02 22:58:53 +0200

    add CoinbaseRetriever to default retriever

--- CryptoPrice/__init__.py
+++ CryptoPrice/__init__.py
@@ -3,14 +3,16 @@ __author__ = "EtWnn"
 
 from CryptoPrice.retrievers.BinanceRetriever import BinanceRetriever
 from CryptoPrice.retrievers.KucoinRetriever import KucoinRetriever
+from CryptoPrice.retrievers.CoinbaseRetriever import CoinbaseRetriever
 from CryptoPrice.retrievers.MetaRetriever import MetaRetriever
 
 
 def get_default_retriever() -> MetaRetriever:
     """
-    Provides a hands on price retriever made from the default BinanceRetriever and the default KucoinRetriever
+    Provides a hands on price retriever made from the default BinanceRetriever, the default KucoinRetriever
+    and the default CoinbaseRetriever
 
     :return: the meta retriever constructed
     :rtype: MetaRetriever
     """
-    return MetaRetriever([BinanceRetriever(), KucoinRetriever()])
+    return MetaRetriever([BinanceRetriever(), KucoinRetriever(), CoinbaseRetriever()])
